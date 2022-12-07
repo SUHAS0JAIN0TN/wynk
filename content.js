fca79745d43f139b0f2edcdd3eb5559ce99a112f
Suhas Jain <suhas@transportsimple.com>
2022-12-07 16:55:15 +0530

    added Plus sign

--- content.js
+++ content.js
@@ -1,4 +1,14 @@
-// console.log("gandu");
-console.log(chrome.storage);
-console.log(localStorage);
-alert("hasas");
+window.addEventListener("load", function () {
+  addToPlayListDiv = document.createElement("div");
+  addToPlayList = document.createElement("span");
+
+  addToPlayList.innerHTML = "+";
+  addToPlayList.style.cssText =
+    "color: rgb(255, 255, 255);font-size: 45px; display: flex; align-items: center;margin-right: 10px;";
+
+  addToPlayListDiv.appendChild(addToPlayList);
+  playBar = document.getElementsByClassName(
+    "flex-grow text-right mr-4 flex justify-end sm:flex-1 h-full"
+  )[0];
+  playBar.insertBefore(addToPlayList, playBar.firstChild);
+});
